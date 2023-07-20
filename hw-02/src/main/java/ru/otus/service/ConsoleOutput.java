1e0bea6aef5f95096350da0922a69aa93f9b7fa4
kalisto <kalisto-87@mail.ru>
2023-07-20 18:55:58 +0500

    Add properties file

--- hw-02/src/main/java/ru/otus/service/ConsoleOutput.java
+++ hw-02/src/main/java/ru/otus/service/ConsoleOutput.java
@@ -1,10 +1,37 @@
 package ru.otus.service;
 
+import ru.otus.domain.Question;
 import ru.otus.domain.Test;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ConsoleOutput implements OutputService {
     @Override
     public void showTest(Test test) {
-        System.out.println(test);
+
+        ConsoleInput input = new ConsoleInput();
+            
+        for (Question question : test.getQuestionList()) {
+
+            boolean isCorreсtInputValue = false;
+            while (!isCorreсtInputValue) {
+                try {
+                    System.out.println(question);
+                    Scanner scanner = new Scanner(System.in);
+                    Integer answer = scanner.nextInt();
+                    if (answer < 1 || answer > question.getAnswerOptions().size()) {
+                        throw new RuntimeException("Answer should be between 1 and ".concat(
+                                Integer.toString(question.getAnswerOptions().size())
+                        ));
+                    }
+                    isCorreсtInputValue = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("You must input only digits");
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+            }
+        }
     }
 }
