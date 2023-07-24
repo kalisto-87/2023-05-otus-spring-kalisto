3a259728af376350b8be306213f1a817c07e2fd1
kalisto <kalisto-87@mail.ru>
2023-07-25 02:29:18 +0300

    fixed

--- hw-03/src/main/java/ru/otus/config/ApplicationConfig.java
+++ hw-03/src/main/java/ru/otus/config/ApplicationConfig.java
@@ -1,16 +1,10 @@
 package ru.otus.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(AppProps.class)
 public class ApplicationConfig {
 
-    @Bean
-    public ApplicationMessage applicationMessage(@Value("${application.message}") String message) {
-        return new ApplicationMessage(message);
-    }
 }
--- hw-03/src/main/java/ru/otus/config/ApplicationMessage.java
+++
@@ -1,4 +0,0 @@
-package ru.otus.config;
-
-public record ApplicationMessage(String message) {
-}
