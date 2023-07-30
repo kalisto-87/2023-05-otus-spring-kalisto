fe5311dc03790bfc2ad1dbebec40e78dd05218c0
kalisto <kalisto-87@mail.ru>
2023-07-30 23:06:07 +0300

    first init commit

---
+++ hw-04/src/main/java/ru/otus/MainApplication.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
+}
