886cee9042698308d5fc9047f0e96464b38b5348
kalisto <kalisto-87@mail.ru>
2023-07-30 23:20:27 +0300

    fix

--- hw-04/src/main/java/ru/otus/MainApplication.java
+++ hw-04/src/main/java/ru/otus/MainApplication.java
@@ -1,9 +1,7 @@
 package ru.otus;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
