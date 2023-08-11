b0420b1a9d31124345bd66932e5e4feb2ffdcfa1
kalisto <kalisto-87@mail.ru>
2023-08-11 17:51:21 +0500

    fix

--- hw-05/src/main/java/ru/otus/MainApplication.java
+++ hw-05/src/main/java/ru/otus/MainApplication.java
@@ -2,11 +2,11 @@ package ru.otus;
 
 import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLException;
 
-@SpringBootConfiguration
+@SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) throws SQLException {
