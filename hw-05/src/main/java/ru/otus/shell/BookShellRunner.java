97cb0d2ffd3ea4d082fefc78f63ce3c59308ed2d
kalisto <kalisto-87@mail.ru>
2023-08-16 14:13:38 +0500

    fixed

--- hw-05/src/main/java/ru/otus/shell/BookShellRunner.java
+++ hw-05/src/main/java/ru/otus/shell/BookShellRunner.java
@@ -12,6 +12,7 @@ import ru.otus.service.AuthorService;
 import ru.otus.service.BookService;
 import ru.otus.service.GenreService;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @ShellComponent
@@ -21,6 +22,10 @@ public class BookShellRunner extends AbstractShellComponent {
     private static final String BOOK_NOT_FOUND = "Book with id = %s not found";
 
     private static final String CHANGES_SUCCESS = "Successfully changed";
+
+    private static final String NO_AUTHORS = "There aren't any authors in the library with name: %s";
+
+    private static final String NO_GENRES = "There aren't any genres in the library with name: %s";
 
     private final BookService bookService;
 
@@ -49,20 +54,26 @@ public class BookShellRunner extends AbstractShellComponent {
     }
 
     @ShellMethod(value = "find all books in the library by author", key = {"b-f-a", "book-find-by-author"})
-    public String findByAuthor(@ShellOption long AuthorId) {
-        Author author = authorService.findById(AuthorId);
-        String books = String.format("List of books written by %s:\n", author.getName());
-        books = books.concat(bookService.findByAuthor(author).stream().
+    public String findByAuthor(@ShellOption String authorName) {
+        List<Author> foundAuthors = authorService.findByName(authorName);
+        if (foundAuthors.size() == 0) {
+            return String.format(NO_AUTHORS, authorName);
+        }
+        String books = String.format("List of books written by %s:\n", foundAuthors.get(0).getName());
+        books = books.concat(bookService.findByAuthor(foundAuthors.get(0)).stream().
                 map(book -> bookConverter.convert(book)).
                 collect(Collectors.joining("\n")));
         return books;
     }
 
     @ShellMethod(value = "find all books in the library by genre", key = {"b-f-g", "book-find-by-genre"})
-    public String findByGenre(@ShellOption long genreId) {
-        Genre genre = genreService.findById(genreId);
-        String books = String.format("List of books genre of %s:\n", genre.getName());
-        books = books.concat(bookService.findByGenre(genre).stream().
+    public String findByGenre(@ShellOption String genreName) {
+        List<Genre> foundGenres = genreService.findByName(genreName);
+        if (foundGenres.size() == 0) {
+            return String.format(NO_GENRES, genreName);
+        }
+        String books = String.format("List of books genre of %s:\n", foundGenres.get(0).getName());
+        books = books.concat(bookService.findByGenre(foundGenres.get(0)).stream().
                 map(book -> bookConverter.convert(book)).
                 collect(Collectors.joining("\n")));
         return books;
