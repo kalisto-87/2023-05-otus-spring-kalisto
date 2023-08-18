3e0c0097ff9892bd86355a70e56caa6fb6e7bbfd
kalisto <kalisto-87@mail.ru>
2023-08-18 16:39:34 +0500

    added domain models

---
+++ hw-06/src/main/java/ru/otus/domain/Author.java
@@ -0,0 +1,24 @@
+package ru.otus.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ hw-06/src/main/java/ru/otus/domain/Comment.java
@@ -0,0 +1,26 @@
+package ru.otus.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Entity
+@Table(name = "book_comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "text")
+    private String text;
+
+    private Book book;
+}
