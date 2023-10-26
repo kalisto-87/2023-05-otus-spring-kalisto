bf97627cf3e185ea906150205d54250ee5c28ddc
kalisto <kalisto-87@mail.ru>
2023-10-27 01:32:14 +0300

    -

--- hw-09/src/main/java/ru/otus/controller/BookController.java
+++ hw-09/src/main/java/ru/otus/controller/BookController.java
@@ -4,10 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.otus.domain.Author;
 import ru.otus.domain.Book;
+import ru.otus.domain.Genre;
+import ru.otus.exception.DataNotFoundException;
 import ru.otus.repository.BookRepository;
+import ru.otus.service.AuthorService;
 import ru.otus.service.BookService;
+import ru.otus.service.GenreService;
 
 import java.util.List;
 
@@ -17,10 +23,25 @@ public class BookController {
 
     private final BookService bookService;
 
+    private final AuthorService authorService;
+
+    private final GenreService genreService;
+
     @GetMapping("/book/list")
     public String bookList(Model model) {
         List<Book> books = bookService.findAll();
         model.addAttribute("books", books);
         return "/book/list";
+    }
+
+    @GetMapping("/book")
+    public String bookEdit(@RequestParam long id, Model model) {
+        Book book = bookService.findById(id).orElseThrow(() -> new DataNotFoundException(""));
+        model.addAttribute("book", book);
+        List<Author> authors = authorService.findAll();
+        model.addAttribute("authors", authors);
+        List<Genre> genres = genreService.findAll();
+        model.addAttribute("genres", genres);
+        return "book/edit";
     }
 }
