e358fa417197191353d8a9495a3137b6b8bb6a17
kalisto <kalisto-87@mail.ru>
2024-01-10 17:21:13 +0500

    fixed

--- hw-12/src/main/java/ru/otus/security/SecurityConfig.java
+++ hw-12/src/main/java/ru/otus/security/SecurityConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +13,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
-                a -> a.requestMatchers("/").permitAll()).formLogin(Customizer.withDefaults());
+        http.csrf().disable()
+                .authorizeHttpRequests(r -> r.requestMatchers("/").permitAll()
+                ).formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- hw-12/src/main/resources/data.sql
+++ hw-12/src/main/resources/data.sql
@@ -46,7 +46,7 @@ insert into book_comments(book_id, text)
 values(2, 'Comment_3');
 
 insert into  users (username, password, role)
-values('user', 'user', 'USER');
+values('user', '$2a$10$3fm18B1.fhQOP7arwaENpOu7DBzHdgExLth4QND8NNBu812PaxtJa', 'USER');
 
 insert into  users (username, password, role)
-values('admin', 'admin', 'ADMIN');
+values('admin', '$2a$10$Qj1PuIenNT03UUU1gJ/LTejolirozL17BL9W9ScDiKHu.qLMoHqPK', 'ADMIN');
