0a591ce3067d70661b5ec96e0e8524105ce48f90
kalisto <kalisto-87@mail.ru>
2024-01-16 01:29:22 +0300

    tests

--- hw-12/src/test/java/ru/otus/rest/AuthorControllerTest.java
+++ hw-12/src/test/java/ru/otus/rest/AuthorControllerTest.java
@@ -1,19 +1,55 @@
 package ru.otus.rest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.dto.AuthorDto;
 import ru.otus.security.SecurityConfig;
 import ru.otus.service.AuthorService;
+import ru.otus.service.AuthorServiceImpl;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthorController.class)
-@Import(SecurityConfig.class)
+@Import({AuthorServiceImpl.class, SecurityConfig.class})
 public class AuthorControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
     @MockBean
-    AuthorService authorService;
+    private AuthorService authorService;
+
+    private AuthorDto authorDto;
+
+    @BeforeEach
+    void setUp() {
+        authorDto = new AuthorDto(10, "New Author");
+    }
+
+    @Test
+    public void shouldAccessAnonymous() throws Exception {
+        mvc.perform(get("/api/author")).andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "user", roles = {"USER"})
+    public void shouldAccessForUser() throws Exception {
+        mvc.perform(get("/api/author").with(csrf())).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", password = "admin", roles = {"ADMIN"})
+    public void shouldAccessForAdmin() throws Exception {
+        mvc.perform(post("/api/author", authorDto).with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
 }
